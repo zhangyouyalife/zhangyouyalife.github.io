8abf4e5918c661ce8dfb32ce4745b93199348ab0
zhangyouyalife <zhang.youya.life@gmail.com>
2026-01-10 22:31:45 +0800

    UNIX philosophy

--- quotes.js
+++ quotes.js
@@ -3,8 +3,8 @@ fetch('./quotes.txt').then(r => {
         r.text().then(quotes_file => {
             console.log(quotes_file);
             var quotes = quotes_file.split("\n");
-            var idx = Math.floor(Math.random() * quotes.length);
-            document.getElementById("quote").textContent = quotes[idx];
+            var idx = Math.floor(Math.random() * (quotes.length - 1));
+            document.getElementById("quote").textContent = quotes[idx].trim();
         })
     }
 });
